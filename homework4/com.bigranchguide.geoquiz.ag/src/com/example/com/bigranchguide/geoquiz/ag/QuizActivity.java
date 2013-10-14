3e4e936ce2e8a4835dc8d4520a7fab0469ca139a
ag993 <albingeorge@Albins-MacBook-Air.local>
2013-10-13 21:21:14 -0400

    Initial commit w/ Buttons
    Initial commit which has the activity defined and buttons have
    listeners. Next step is to add the toast.

---
+++ homework4/com.bigranchguide.geoquiz.ag/src/com/example/com/bigranchguide/geoquiz/ag/QuizActivity.java
@@ -0,0 +1,45 @@
+package com.example.com.bigranchguide.geoquiz.ag;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class QuizActivity extends Activity {
+	
+	private Button mTrueButton;
+	private Button mFalseButton;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_quiz);
+		
+		mTrueButton = (Button)findViewById(R.id.true_button);
+		mTrueButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//Does nothing yet, but soon!
+			}
+		});
+		
+		mFalseButton = (Button)findViewById(R.id.false_button);
+		mFalseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.quiz, menu);
+		return true;
+	}
+
+}
