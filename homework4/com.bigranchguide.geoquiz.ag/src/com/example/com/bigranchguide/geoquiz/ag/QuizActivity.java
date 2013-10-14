c6ddfda0562aa071ed6474b9aac6de7ff8116ad9
ag993 <albingeorge@Albins-MacBook-Air.local>
2013-10-13 22:51:29 -0400

    Linked Toasts to user's answer
    checkAnswer function now compares the users choice (depending on which
    button the pressed) to the value stored in the array. Based on which,
    it adjusts the Toast.

--- homework4/com.bigranchguide.geoquiz.ag/src/com/example/com/bigranchguide/geoquiz/ag/QuizActivity.java
+++ homework4/com.bigranchguide.geoquiz.ag/src/com/example/com/bigranchguide/geoquiz/ag/QuizActivity.java
@@ -30,6 +30,19 @@ public class QuizActivity extends Activity {
 		mQuestionTextView.setText(question);
 	}
 	
+	private void checkAnswer(boolean userPressedTrue) {
+		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+		int messageResId = 0;
+		
+		if(userPressedTrue == answerIsTrue) {
+			messageResId = R.string.correct_toast;
+		} else {
+			messageResId = R.string.incorrect_toast;
+		}
+		
+		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -42,9 +55,7 @@ public class QuizActivity extends Activity {
 		mTrueButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(QuizActivity.this, 
-						R.string.incorrect_toast, 
-						Toast.LENGTH_SHORT).show();
+				checkAnswer(true);
 			}
 		});
 		
@@ -53,9 +64,7 @@ public class QuizActivity extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(QuizActivity.this, 
-						R.string.correct_toast, 
-						Toast.LENGTH_SHORT).show();
+				checkAnswer(false);
 			}
 		});
 		
