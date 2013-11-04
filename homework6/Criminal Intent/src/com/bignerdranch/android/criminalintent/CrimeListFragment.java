36cf942b223750861620591c4d1739c7a730d074
ag993 <albingeorge@ulsecure-belknap-000009.wireless.louisville.edu>
2013-11-04 14:02:22 -0500

    Chapter 10 complete
    YAY!!!

--- homework6/Criminal Intent/src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ homework6/Criminal Intent/src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.bignerdranch.android.ciminalintent.R;
 
@@ -17,9 +20,41 @@ public class CrimeListFragment extends ListFragment {
 
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
-		Crime c = (Crime)(getListAdapter()).getItem(position);
+		Crime c = (Crime)getListAdapter().getItem(position);
 		Log.d(TAG, c.getmTitle() + " was clicked");
 	}
+	
+	private class CrimeAdapter extends ArrayAdapter<Crime>{
+		public CrimeAdapter(ArrayList<Crime> crimes) {
+			super(getActivity(), 0, crimes);
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			//If we weren't given a view, inflate one
+			if (convertView == null){
+				convertView = getActivity().getLayoutInflater()
+						.inflate(R.layout.list_item_crime, null);
+			}
+			//Configure the view for this crime
+			Crime c = getItem(position);
+			
+			TextView titleTextView =
+					(TextView)convertView.findViewById(R.id.crime_list_item_titleTextView);
+			titleTextView.setText(c.getmTitle());
+			
+			TextView dateTextView =
+					(TextView)convertView.findViewById(R.id.crime_list_item_dateTextView);
+			dateTextView.setText(c.getmDate().toString());
+			
+			CheckBox solvedCheckBox =
+					(CheckBox)convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+			solvedCheckBox.setChecked(c.getmSolved());
+			
+			return convertView;
+	}
+		}
+	
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -27,10 +62,7 @@ public class CrimeListFragment extends ListFragment {
 		getActivity().setTitle(R.string.crimes_title);
 		mCrimes = CrimeLab.get(getActivity()).getCrimes();
 		
-		ArrayAdapter<Crime> adapter = 
-				new ArrayAdapter<Crime>(getActivity(),
-						android.R.layout.simple_list_item_1,
-						mCrimes);
+		CrimeAdapter adapter = new CrimeAdapter(mCrimes);
 		setListAdapter(adapter);
 	}
 	
