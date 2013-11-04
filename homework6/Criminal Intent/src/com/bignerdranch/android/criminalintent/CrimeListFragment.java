f1b8e15d3ff5de30f78a07d170713892c09226ee
ag993 <albingeorge@ulsecure-belknap-005137.wireless.louisville.edu>
2013-11-04 10:24:15 -0500

    Added ArrayAdapter to CrimeListFragment

--- homework6/Criminal Intent/src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ homework6/Criminal Intent/src/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
 
 import com.bignerdranch.android.ciminalintent.R;
 
@@ -15,6 +16,12 @@ public class CrimeListFragment extends ListFragment {
 		super.onCreate(savedInstanceState);
 		getActivity().setTitle(R.string.crimes_title);
 		mCrimes = CrimeLab.get(getActivity()).getCrimes();
+		
+		ArrayAdapter<Crime> adapter = 
+				new ArrayAdapter<Crime>(getActivity(),
+						android.R.layout.simple_list_item_1,
+						mCrimes);
+		setListAdapter(adapter);
 	}
 	
 }
