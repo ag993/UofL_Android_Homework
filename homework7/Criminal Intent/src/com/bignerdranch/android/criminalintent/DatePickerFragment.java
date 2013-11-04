f303c05fd20bdb1e02af9a72bfbca8249837d6e8
ag993 <albingeorge@ulsecure-belknap-000009.wireless.louisville.edu>
2013-11-04 16:40:18 -0500

    Added Dialog Fragment

--- homework7/Criminal Intent/src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ homework7/Criminal Intent/src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -1,7 +1,20 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
+import com.bignerdranch.android.ciminalintent.R;
+
 public class DatePickerFragment extends DialogFragment {
+	
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		return new AlertDialog.Builder(getActivity())
+		.setTitle(R.string.date_picker_title)
+		.setPositiveButton(android.R.string.ok,null)
+		.create();
+	}
 
 }
