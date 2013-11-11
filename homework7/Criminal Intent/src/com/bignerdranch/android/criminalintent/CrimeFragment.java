4a8166b6087ae8cea15f302472378d7e53509cd2
ag993 <albingeorge@Albins-MacBook-Air.local>
2013-11-11 13:55:27 -0500

    Added parent activity metadata and what not

--- homework7/Criminal Intent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ homework7/Criminal Intent/src/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -4,14 +4,18 @@ package com.bignerdranch.android.criminalintent;
 import java.util.Date;
 import java.util.UUID;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -32,12 +36,28 @@ public class CrimeFragment extends Fragment {
 	private static final int REQUEST_DATE = 0;
 	
 	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()) {
+		case android.R.id.home:
+			if (NavUtils.getParentActivityName(getActivity()) != null) {
+				NavUtils.navigateUpFromSameTask(getActivity());
+			}
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		
 		UUID crimeID = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
 		
 		mCrime = CrimeLab.get(getActivity()).getCrime(crimeID);
+		
+		setHasOptionsMenu(true);
 	}
 	
 	public static CrimeFragment newInstance(UUID crimeID) {
@@ -54,9 +74,17 @@ public class CrimeFragment extends Fragment {
 		mDateButton.setText(mCrime.getmDate().toString());
 	}
 	
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
 		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			if (NavUtils.getParentActivityName(getActivity()) != null) {
+				
+			getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+		}}
+		
 		
 		EditText mTitleField = (EditText)v.findViewById(R.id.crime_title);
 		mTitleField.setText(mCrime.getmTitle());
